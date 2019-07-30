efcf4271a797d4910dcfc42068224fb038c8709b
evanandrewson <evanandrewson@gmail.com>
2019-07-30 09:10:48 -0700

    added array of products

---
+++ src/data/products.js
@@ -0,0 +1,144 @@
+const bananaSlicer = {
+    id: 'banana-slicer',
+    image: './assets./products/banana-slicer.jpg',
+    name: 'Banana Slicer'
+};
+
+const bathroomTabletStand = {
+    id: 'bathroom-tablet-stand',
+    image: './assets./products/bathroom-tablet-stand.jpg',
+    name: 'Bathroom Tablet Stand'
+};
+
+const breakfastMakingMachine = {
+    id: 'breakfast-making-machine',
+    image: './assets./products/breakfast-making-machine.jpg',
+    name: 'Breakfast Making Machine'
+};
+
+const cthulhuActionFigure = {
+    id: 'cthulhu-action-figure',
+    image: './assets./products/cthulhu-action-figure.jpg',
+    name: 'Cthulhu Action Figure'
+};
+
+const dogDuckBill = {
+    id: 'dog-duck-bill',
+    image: './assets./products/dog-duck-bill.jpg',
+    name: 'Dog Duck Bill'
+};
+
+const dragonMeat = {
+    id: 'dragon-meat',
+    image: './assets./products/dragon-meat.jpg',
+    name: 'Dragon Meat'
+};
+
+const meatballBubblegum = {
+    id: 'meatball-bubblegum',
+    image: './assets./products/meatball-bubblegum.jpg',
+    name: 'Meatball Bubblegum'
+};
+
+const openToedBoots = {
+    id: 'open-toed-boots',
+    image: './assets./products/open-toed-boots.jpg',
+    name: 'Open Toed Boots'
+};
+
+const petSweep = {
+    id: 'pet-sweep',
+    image: './assets./products/pet-sweep.jpg',
+    name: 'Pet Sweep'
+};
+
+const pizzaScissors = {
+    id: 'pizza-scissors',
+    image: './assets./products/pizza-scissors.jpg',
+    name: 'Pizza Scissors'
+};
+
+const retroChair = {
+    id: 'retro-chair',
+    image: './assets./products/retro-chair.jpg',
+    name: 'Retro Chair'
+};
+
+const sharkSleepingBag = {
+    id: 'shark-sleeping-bag',
+    image: './assets./products/shark-sleeping-bag.jpg',
+    name: 'Shark Sleeping Bag'
+};
+
+const silverwarePenCap = {
+    id: 'silverware-pen-cap',
+    image: './assets./products/silverware-pen-cap.jpg',
+    name: 'Silverware Pen Cap'
+};
+
+const starWarsBag = {
+    id: 'star-wars-bag',
+    image: './assets./products/star-wars-bag.jpg',
+    name: 'Star Wars Bag'
+};
+
+const sweepOnesie = {
+    id: 'sweep-onesie',
+    image: './assets./products/sweep-onesie.png',
+    name: 'Sweep Onesie'
+};
+
+const tauntaunSleepingBag = {
+    id: 'tauntaun-sleeping-bag',
+    image: './assets./products/tauntaun-sleeping-bag.jpg',
+    name: 'Tauntaun Sleeping Bag'
+};
+
+const unicornMeat = {
+    id: 'unicorn-meat',
+    image: './assets./products/unicorn-meat.jpg',
+    name: 'Unicorn Meat'
+};
+
+const usbTentacle = {
+    id: 'usb-tentacle',
+    image: './assets./products/usb-tentacle.gif',
+    name: 'USB Tentacle'
+};
+
+const waterCan = {
+    id: 'water-can',
+    image: './assets./products/water-can.jpg',
+    name: 'Water Can'
+};
+
+const wineGlass = {
+    id: 'wine-glass',
+    image: './assets./products/wine-glass.jpg',
+    name: 'Wine Glass'
+};
+
+const products = [
+    bananaSlicer,
+    bathroomTabletStand,
+    breakfastMakingMachine,
+    cthulhuActionFigure,
+    dogDuckBill,
+    dragonMeat,
+    meatballBubblegum,
+    openToedBoots,
+    petSweep,
+    pizzaScissors,
+    retroChair,
+    sharkSleepingBag,
+    silverwarePenCap,
+    starWarsBag,
+    sweepOnesie,
+    tauntaunSleepingBag,
+    unicornMeat,
+    usbTentacle,
+    waterCan,
+    wineGlass
+];
+
+export default products;
